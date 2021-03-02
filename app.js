373580e735fb65c505c6b5e24a5ae20125f7bb61
Potter <Potter@Hogwarts.edu>
2021-03-02 03:45:43 +0000

    Home page

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) =>
-{
-    res.send('Hello, World!')
-})
+app.use(express.static('public'))
 
+var indexRouter = require('./routes/index.js')
+
+app.use('/', indexRouter)
 module.exports = app
---
+++ controllers/indexController.js
@@ -0,0 +1,2 @@
+exports.index = (req, res) => res.render('index/index.ejs')
+exports.login = (req, res) => res.render('index/login.ejs')
