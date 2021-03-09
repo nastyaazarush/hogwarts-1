5cd9f98686532c57196a9ba90484df12fe098064
nastyaazarush <aazarushkina@bk.ru>
2021-03-09 04:23:05 +0000

    Страницу вдоха создали

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,2 +1,7 @@
 exports.index = (req, res) => res.render('index/index.ejs')
 exports.login = (req, res) => res.render('index/login.ejs')
+exports.api_login = (req, res) =>
+{
+    let {username, password} = req.body
+    
+}
